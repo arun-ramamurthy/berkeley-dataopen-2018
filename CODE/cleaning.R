1910ff08dc13a136d5498686e80d5e99417ce956
Biyonka Liang <Biyonka@airbears2-10-142-35-42.airbears2.1918.berkeley.edu>
2018-09-08 09:08:02 -0700

    food venues cleaning

--- CODE/cleaning.R
+++ CODE/cleaning.R
@@ -1,5 +1,4 @@
 ## A suite of functions for cleaning and merging datasets.
-
 # Load convenience functions,
 library(here)
 here = here::here
@@ -14,17 +13,19 @@ clean_311_service_requests <- function(raw) {
   raw
 }
 
+food_venues = read.csv("~/Downloads/Datathon_Materials/food_venues.csv.gz", sep = ',')
 clean_food_venues <- function(raw) {
-  raw %>% mutate(key = as.character(business_name),
+  raw %>% transmute(business_name = as.character(business_name),
                  street_address = as.character(street_address),
                  city = as.character(city),
                  state = as.character(state),
                  country = as.character(country),
-                 latitude = as.numeric(latitude),
-                 longitude = as.numeric(longitude),
+                 lat = as.numeric(latitude),
+                 long = as.numeric(longitude),
                  categories = as.character(categories),
                  type = as.character(type))
 }
+cleaned_food_venues = clean_food_venues(food_venues)
 
 clean_food_inspections <- function(raw) {
   raw
@@ -48,7 +49,7 @@ clean_geo <- function(raw) {
 make_design_matrix.raw <- function(dir = "DATA") {
   dir <- glue("{here()}/{dir}/")
   service_requests <- read_csv(dir + "") %>% clean_311_service_requests()
-  food_venues <- read_csv(dir + "") %>% clean_food_venues()
+  food_venues <- read_csv(dir + "food_venues.csv.gz") %>% clean_food_venues()
   food_inspections <- read_csv(dir + "") %>% clean_food_inspections()
   clean_health_indicators <- read_csv(dir + "") %>% clean_health_indicators()
   clean_county_demographics <- read_csv(dir + "") %>% clean_county_demographics()
