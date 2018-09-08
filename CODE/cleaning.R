3273b9529b7f2cf28749e01b5372453a6690239e
Arun Ramamurthy <contact@arun.run>
2018-09-08 11:42:08 -0700

    Added demo_state. TODO: nta_demographics, geo, merging

--- CODE/cleaning.R
+++ CODE/cleaning.R
@@ -15,20 +15,20 @@ clean_311_service_requests <- function(raw) {
   raw %>%
     transmute(key = as.character(unique_key),
               created = mdy_hms(created_date),
-              closed = mdy_hms(closed_date),
-              due = mdy_hms(due_date),
-              resolved = mdy_hms(resolution_date),
-              agency = as.factor(agency_acronym),
-              agency_name = as.character(agency_name),
+              ## closed = mdy_hms(closed_date),
+              ## due = mdy_hms(due_date),
+              ## resolved = mdy_hms(resolution_date),
+              ## agency = as.factor(agency_acronym),
+              ## agency_name = as.character(agency_name),
               complaint_type = as.factor(complaint_type),
               description = as.character(descriptor),
               location_type = as.factor(location_type),
-              zip = as.character(incident_zip),
-              address = as.character(incident_address),
-              street = as.character(street_name),
-              city = as.character(city),
-              status = as.factor(status),
-              board = as.character(community_board),
+              ## zip = as.character(incident_zip),
+              ## address = as.character(incident_address),
+              ## street = as.character(street_name),
+              ## city = as.character(city),
+              ## status = as.factor(status),
+              ## board = as.character(community_board),
               borough = as.factor(borough),
               lat = as.numeric(latitude),
               lon = as.numeric(longitude))
@@ -46,7 +46,11 @@ clean_food_venues <- function(raw) {
                  type = as.character(type))
 }
 
-clean_food_inspections <- function(raw) {
+clean_nta_demographics <- function(raw) {
+  raw
+}
+
+clean_geo <- function(raw) {
   raw
 }
 
@@ -56,7 +60,7 @@ clean_health_indicators <- function(raw) {
   raw %>%
     transmute(county = as.factor(county_name),
               topic = as.factor(health_topic),
-              indicator = as.character(topic),
+              indicator = as.character(indicator),
               count = as.integer(event_count),
               denom = as.integer(avg_num_of_denominator),
               unit = as.factor(unit_measurement),
@@ -68,11 +72,49 @@ clean_health_indicators <- function(raw) {
 }
 
 clean_county_demographics <- function(raw) {
-  raw
+  raw %>%
+    mutate(borough = geography %>%
+             recode("Richmond County, New York" = "Staten Island",
+                    "New York County, New York" = "Manhattan",
+                    "Bronx County, New York" = "Bronx",
+                    "Kings County, New York" = "Brooklyn",
+                    "Queens County, New York" = "Queens",
+                    "New York" = "New York (state)",
+                    .default = NA_character_) %>%
+                as.factor(),
+           population = as.integer(population),
+           prop_w_health_insurance = pop_w_health_insurance / population,
+           prop_w_private_health = pop_w_private_health / population,
+           prop_w_public_health = pop_w_public_coverage / population,
+           prop_w_no_health = pop_no_health_insurance / population) %>%
+    select(- geography) %>%
+    drop_na(borough) %>%
+    select(borough, year,
+           total_households, population,
+           ends_with("household_income"), starts_with("mean"),
+           food_stamp_benefits,
+           starts_with("prop"))
 }
 
-clean_geo <- function(raw) {
-  raw
+clean_county_demographics_brackets <- function(raw) {
+  raw %>%
+    mutate(borough = geography %>%
+             recode("Richmond County, New York" = "Staten Island",
+                    "New York County, New York" = "Manhattan",
+                    "Bronx County, New York" = "Bronx",
+                    "Kings County, New York" = "Brooklyn",
+                    "Queens County, New York" = "Queens",
+                    "New York" = "New York (state)",
+                    .default = NA_character_) %>%
+             as.factor()) %>%
+    select(- geography) %>%
+    drop_na(borough) %>%
+    gather(income_bracket, households, starts_with("$")) %>%
+    mutate(income_bracket = as.factor(income_bracket),
+           proportion = households / total_households) %>%
+    select(borough, year,
+           income_bracket, proportion, households, total_households,
+           median_household_income, mean_household_income, mean_earnings)
 }
 ##################
 
@@ -81,10 +123,9 @@ clean_geo <- function(raw) {
 make_design_matrix.raw <- function(dir = "DATA/Datathon Materials") {
   dir <- glue("{here()}/{dir}/")
   service_requests <- read_csv(dir + "311_service_requests.csv.gz") %>% clean_311_service_requests()
-  food_venues <- read_csv(dir + "") %>% clean_food_venues()
-  food_inspections <- read_csv(dir + "") %>% clean_food_inspections()
+  food_venues <- read_csv(dir + "food_venues.csv.gz") %>% clean_food_venues()
   health_indicators <- read_csv(dir + "community_health.csv") %>% clean_health_indicators()
-  county_demographics <- read_csv(dir + "") %>% clean_county_demographics()
+  county_demographics <- read_csv(dir + "demographics.csv") %>% clean_county_demographics()
   design_matrix <-
     list(food_venues, food_inspections) %>% reduce(left_join, by = c("latitude", "longitude"))
 }
